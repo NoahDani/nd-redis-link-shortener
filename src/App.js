ed5a5d3fed8ec0063f45cce40dc9c6af65797876
Noah Daniels <n.d.kahler@gmail.com>
2023-02-06 18:57:41 -0600

    adding a state to the react form and additional quality of life changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="banner">
-        Redis Link Shortener
-      </div>
-      <div className='link-url-box'>
-        <form>
-          <input placeholder='Paste URL here!'/>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 'Paste URL Here!'
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(){
+
+  }
+  
+  render () {
+    return (
+    <div className="app">
+        <div className="banner">
+          Redis Link Shortener
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className='link-url-box'>
+            <input value={this.state.value} onChange={this.handleChange} />
+          </div>
+          <div className='link-url-box-button'>
+            <input type='submit' value='Submit' />
+          </div>
         </form>
+        <div className='shortened-link'>
+          Your Shortened URL is:
+          {this.state.value}
+        </div>
       </div>
-      <div className='link-url-box-button'>
-        <button type='submit'>Submit</button>
-      </div>
-      <div className='shortened-link'>
-        Your Shortened URL is: 
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
